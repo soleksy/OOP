e4de9df508f9356e94e412306c75edc27b2b018a
Szymon Oleksy <Szymon.Oleksy@student.uj.edu.pl>
2021-04-01 11:36:58 +0200

    add comment controller and its functionalities

--- exercises/02/Sources/App/routes.swift
+++ exercises/02/Sources/App/routes.swift
@@ -5,54 +5,10 @@ func routes(_ app: Application) throws {
 
     try app.register(collection: UserController())
     try app.register(collection: PostController())
+    try app.register(collection: CommentController())
 
-    // Comments
-
-    //get all comments
-    app.get("comments") { req in 
-        Comment.query(on: req.db).all()
-    }
-    //get comments by id
-    app.get("comments" , ":commentId") { req -> EventLoopFuture<Comment> in
-
-        Comment.find(req.parameters.get("commentId") , on: req.db)
-        .unwrap(or: Abort(.notFound))
-    }
-
-    //add a comment
-     app.post("comments") { req -> EventLoopFuture<Comment> in
-        let comment = try req.content.decode(Comment.self)
-        return comment.create(on: req.db).map{ comment }
-    }
-
-    //update a comment (edit)
-    struct UpdateComment: Content {
-        var id: UUID?
-        var body: String
-    }
-
-    app.put("comments") { req -> EventLoopFuture<HTTPStatus> in
-
-        let comment = try req.content.decode(UpdateComment.self)
-
-        return Comment.find(comment.id , on: req.db)
-        .unwrap(or: Abort(.notFound))
-        .flatMap{
-            $0.body = comment.body
-            return $0.update(on: req.db).transform(to: .ok)
-        }
-    }
-
-    //delete a comment
-
-    app.delete("comments", ":commentId") {req -> EventLoopFuture<HTTPStatus> in
-
-        Comment.find(req.parameters.get("commentId") , on: req.db)
-        .unwrap(or: Abort(.notFound))
-        .flatMap{
-            $0.delete(on: req.db)
-        }.transform(to: .ok)
-    }
-
-
+    app.get() { req -> EventLoopFuture<View> in 
+        return req.view.render("home")
+    } 
+   
 }
