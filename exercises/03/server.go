5b02b205c643d7a32ad7d2ef50058f857471cfbc
Szymon Oleksy <Szymon.Oleksy@student.uj.edu.pl>
2021-04-16 22:01:31 +0200

    add user crud , add endpoint to view all users

--- exercises/03/server.go
+++ exercises/03/server.go
@@ -12,14 +12,29 @@ import (
 func main() {
 
 	DB := singleton.GetInstance()
-	
+
 	DB.SetDBName("users.db")
 
 	DB.Open()
 
+	
+	//DB.UpdateUser(4,"Vladimir")
+	//DB.DeleteUser(1)
+	//DB.DeleteUser(2)
+	//DB.DeleteUser(3)
+
+	DB.AddUser("Jacek")
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+
+	e.GET("/users", func(c echo.Context) error {
+		users := DB.GetAllUsers()
+
+		if err := c.Bind(users); err != nil{
+			return err
+		}
+		return c.JSON(http.StatusCreated, users)
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
--- exercises/03/singleton/DB.go
+++ exercises/03/singleton/DB.go
@@ -6,12 +6,23 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type Item struct {
+	Name string `json:"name"`
+	ID int `json:"id"`
+}
+
 type Singleton interface {
 	Open() string
 	Close() string
 	SetDBName(name string) string
 
-	GetAllUsers() []string
+	AddUser(name string) string
+	DeleteUser(id int) string
+	UpdateUser(id int , name string) string
+	GetUser(id int) string
+
+	GetAllUsers() []Item
+
 }
 
 type singleton struct {
@@ -79,9 +90,74 @@ func (s *singleton) SetDBName(name string) string {
 	}
 }
 
-func (s *singleton) GetAllUsers() []string {
+func (s *singleton) GetAllUsers() []Item  {
+	items := [] Item{}
+	rows , err := s.DB.Query("SELECT * FROM users")
 
-	users := []string{"user1","user2"}
+	checkErr(err)
 
-	return users
+	defer rows.Close()
+	var id int
+	var name string
+
+	for rows.Next() {
+		rows.Scan(&id, &name)
+		items = append(items, Item{name,id})
+	}
+
+	return items
+}
+
+func (s *singleton) AddUser(name string) string {
+	stmt , err := s.DB.Prepare(`INSERT INTO users (name) values (?)`)
+
+	checkErr(err)
+	
+	_ , err = stmt.Exec(name)
+
+	checkErr(err)
+	return "User added"
+
+}
+
+func (s *singleton) GetUser(id int) string {
+
+	user , err := s.DB.Query("SELECT name FROM users WHERE id = ?" , id)
+
+	checkErr(err)
+		
+	defer user.Close()
+	var name string
+	
+	for user.Next(){
+		user.Scan(&name)
+	}
+
+	return name
+
+}
+
+func (s *singleton) UpdateUser(id int , name string) string {
+	stmt, err := s.DB.Prepare("UPDATE users SET name=? WHERE id=?")
+	
+	checkErr(err)
+
+	_ , err = stmt.Exec(name , id)
+
+	checkErr(err)
+
+	return "User information updated"
+
+}
+
+func (s *singleton) DeleteUser(id int) string {
+	stmt, err := s.DB.Prepare("DELETE FROM users WHERE id=?")
+	
+	checkErr(err)
+
+	_ , err = stmt.Exec(id)
+
+	checkErr(err)
+
+	return "User deleted"
 }
