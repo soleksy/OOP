a0db454702442e435635634b7af7018d01296852
Szymon Oleksy <Szymon.Oleksy@student.uj.edu.pl>
2021-04-21 23:54:40 +0200

    adding exercise 4 solution

---
+++ exercises/04/app/src/test/kotlin/pl/mypackage/myApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.mypackage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class myApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
